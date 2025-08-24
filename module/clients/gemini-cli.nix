818be359dcc85b3de7d7d0dfdb34d3188322a0eb
Pieter Pel <pelpieter@gmail.com>
2025-08-24 15:27:15 +0200

    fixed mkmerge

--- module/clients/gemini-cli.nix
+++ module/clients/gemini-cli.nix
@@ -8,7 +8,10 @@
 let
   globalCfg = config.programs.mcpix;
   cfg = config.programs.mcpix.targets.gemini-cli;
-  mergedServers = lib.mkMerge globalCfg.globalServers cfg.servers;
+  mergedServers = lib.mkMerge [
+    globalCfg.globalServers
+    cfg.servers
+  ];
   mcpConfig = mcp-servers-nix.lib.mkConfig pkgs mergedServers;
 in
 {
