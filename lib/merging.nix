ef8f6216780cd6d4f0527157f1a817c2671cd124
Pieter Pel <pelpieter@gmail.com>
2025-08-24 19:24:50 +0200

    let's try again

--- lib/merging.nix
+++ lib/merging.nix
@@ -10,7 +10,7 @@
     let
       mergedServers = lib.recursiveUpdate globalCfg.servers (cfg.servers or { });
       mcpConfig = mcp-servers-nix.lib.mkConfig pkgs mergedServers;
-      result = if mergedServers == { } then { servers = { }; } else mcpConfig;
+      result = if mergedServers == { } then { mcpServers = { }; } else mcpConfig;
     in
     result.mcpServers;
 }
