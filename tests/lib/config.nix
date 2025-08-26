69e5fffe6f242adb1c2d334baf015527cb85489c
Pieter Pel <pelpieter@gmail.com>
2025-08-26 13:19:54 +0200

    All tests pass now

--- tests/lib/config.nix
+++ tests/lib/config.nix
@@ -6,13 +6,42 @@
 let
   mcp-servers-nix = inputs.mcp-servers-nix;
   mkConfig = mcp-servers-nix.lib.mkConfig;
-  stub = _: true; # NOTE: just test if the evaluation goes through
+  stub = input: input != null; # NOTE: just test if the evaluation goes through
 in
 {
   testMcpServersNixEmpty = {
     expr = stub (
       mkConfig pkgs {
         programs = { };
+        # NOTE: without this, there is an error
+        settings.servers = { };
+      }
+    );
+    expected = true;
+  };
+
+  testMcpServersNixFetch = {
+    expr = stub (
+      mkConfig pkgs {
+        programs = {
+          fetch.enable = true;
+        };
+      }
+    );
+    expected = true;
+  };
+
+  testMcpServersNixCustom = {
+    expr = stub (
+      mkConfig pkgs {
+        settings = {
+          servers = {
+            mockServer = {
+              command = "echo";
+              args = [ "hello" ];
+            };
+          };
+        };
       }
     );
     expected = true;
