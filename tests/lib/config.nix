ce7ded64cff3fd15d015705b1a0d2c3695915128
Pieter Pel <pelpieter@gmail.com>
2025-08-27 14:40:01 +0200

    let's try updated acions

--- tests/lib/config.nix
+++ tests/lib/config.nix
@@ -9,16 +9,16 @@ let
   stub = input: input != null; # NOTE: just test if the evaluation goes through
 in
 {
-  # testMcpServersNixEmpty = {
-  #   expr = stub (
-  #     mkConfig pkgs {
-  #       programs = { };
-  #       # NOTE: without this, there is an error
-  #       settings.servers = { };
-  #     }
-  #   );
-  #   expected = true;
-  # };
+  testMcpServersNixEmpty = {
+    expr = stub (
+      mkConfig pkgs {
+        programs = { };
+        # NOTE: without this, there is an error
+        settings.servers = { };
+      }
+    );
+    expected = true;
+  };
 
   testMcpServersNixFetch = {
     expr = stub (
